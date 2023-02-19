7093e32a601c7d208025c698e6f987ade919c8a4
Elias <eliashelin98@gmail.com>
2023-02-19 22:51:00 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
 
 export default function App() {
+
+  const [selectedcurrency, setSelectedCurrency] = useState();
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState();
+  const [converted, setResult] = useState();
+
+  var myHeaders = new Headers();
+  myHeaders.append("apikey", "NSeDMsWm8uojUxLoCVkYQonFeVNTL8CW");
+
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+
+  useEffect(() => {
+    fetch(`https://api.apilayer.com/exchangerates_data/symbols`, requestOptions)
+      .then(response => response.json())
+      .then(responseJson => setCurrencies(Object.keys(responseJson.symbols)))
+      .catch(error => console.error(error));
+
+  }, []);
+
+
+
+  const fetchConvert = () => {
+    fetch(`https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=${selectedcurrency}&amount=${amount}`, requestOptions)
+      .then(response => response.json())
+      .then(responseJson => setResult("="+responseJson.result.toFixed(2)+"€"))
+      .catch(error => {
+        Alert.alert('Error', error.message);
+      });
+
+  }
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.item}>Money converter</Text>
+      
+      <View>
+        <View>
+          <TextInput
+            style={styles.item}
+            placeholder={"enter amount here"}
+            keyboardType={"number-pad"}
+            onChangeText={amount => setAmount(amount)}
+            value={amount}
+          />
+        </View>
+        <View style={styles.dropdown}>
+          <Picker style={styles.picker}
+
+            selectedValue={selectedcurrency}
+            onValueChange={(itemValue) => setSelectedCurrency(itemValue)}
+          >
+            {
+              currencies.map((item) => (
+                <Picker.Item key={item} label={item} value={item} />
+              ))
+            }
+          </Picker>
+        </View>
+        <Text style={styles.item}>{converted}</Text>
+      </View>
+
+      <Button
+        title='Convert'
+        onPress={fetchConvert}
+      />
     </View>
   );
 }
@@ -17,4 +87,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  picker: {
+    height: 100,
+    width: "100%",
+    color: 'black',
+    justifyContent: 'center',
+  },
+
+  item: {
+    fontSize: 20,
+    marginTop: 20,
+  },
+  input: {
+    border:"2px",
+    borderColor:"black",
+    borderStyle:"solid"
+  },
+  dropdown: {
+    maxHeight:50,
+  }
 });
